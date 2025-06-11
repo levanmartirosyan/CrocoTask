1d23b682919cd7f5a7bb1901009c95eb396721d7
Levan <levan.martirosyan31@gmail.com>
2025-06-11 17:49:05 +0400

    fixed bugs

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,6 +9,8 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent implements OnInit {
+  constructor(private renderer: Renderer2) {}
+
   ngOnInit(): void {
     this.getCurrentTime();
   }
@@ -24,6 +26,11 @@ export class HeaderComponent implements OnInit {
 
   toggleBurgerMenu() {
     this.burgerMenu = !this.burgerMenu;
+    if (this.burgerMenu) {
+      this.renderer.addClass(document.body, 'noScroll');
+    } else {
+      this.renderer.removeClass(document.body, 'noScroll');
+    }
   }
 
   closeBurgerMenu() {
