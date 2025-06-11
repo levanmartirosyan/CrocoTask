419a59e345d8187741a85e2b3ada8070d6f88837
Levan <levan.martirosyan31@gmail.com>
2025-06-11 15:34:36 +0400

    test

--- src/app/services/tools.service.ts
+++ src/app/services/tools.service.ts
@@ -1,14 +1,10 @@
-import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ToolsService {
-  constructor(private rendererFactory: RendererFactory2) {
-    this.renderer = rendererFactory.createRenderer(null, null);
-  }
-
-  private renderer: Renderer2;
+  constructor() {}
 
   busyRequestCount = 0;
   showCustomLoader = false;
@@ -16,7 +12,6 @@ export class ToolsService {
   busy() {
     this.busyRequestCount++;
     this.showCustomLoader = true;
-    this.renderer.addClass(document.body, 'noScroll');
   }
 
   idle() {
@@ -24,7 +19,6 @@ export class ToolsService {
     if (this.busyRequestCount <= 0) {
       this.busyRequestCount = 0;
       this.showCustomLoader = false;
-      this.renderer.removeClass(document.body, 'noScroll');
     }
   }
 }
