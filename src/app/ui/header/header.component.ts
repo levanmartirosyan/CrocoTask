899be581e8c1da5f9c2460a6cd6eef651032439f
Levan <levan.martirosyan31@gmail.com>
2025-06-11 18:03:41 +0400

    hot fix

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, Renderer2 } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +10,17 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent implements OnInit {
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2, private router: Router) {}
 
   ngOnInit(): void {
     this.getCurrentTime();
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.burgerMenu = false;
+        this.renderer.removeClass(document.body, 'noScroll');
+      });
   }
 
   public currentTime: Date = new Date();
