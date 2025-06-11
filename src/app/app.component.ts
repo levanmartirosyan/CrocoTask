7fcd21fd8a63d98f8a3c0567e006eaa97fd6899d
Levan <levan.martirosyan31@gmail.com>
2025-06-11 17:55:37 +0400

    fixed bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './ui/header/header.component';
 import { NavigationComponent } from './ui/navigation/navigation.component';
 import { FooterComponent } from './ui/footer/footer.component';
-import { ViewportScroller } from '@angular/common';
-import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,19 +15,6 @@ import { filter } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  constructor(
-    private viewportScroller: ViewportScroller,
-    private router: Router
-  ) {}
-
-  ngOnInit(): void {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe(() => {
-        this.viewportScroller.scrollToPosition([0, 0]);
-      });
-  }
-
+export class AppComponent {
   title = 'croco-task';
 }
